631348c6c7fcd9ad08fa437c1acebf9dfd0444b6
Serkan <94819174+Serkangur@users.noreply.github.com>
2021-11-23 07:01:55 +0300

    C# While Loop
    C# While Loop

---
+++ while/while/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            while (a >= -2)
+            {
+                Console.WriteLine(a);
+                a = a - 2;
+            }
+            Console.ReadLine();
+        }
+    }
+}
